ab6e8d3bcbbc01b4242e3efc6a22908bfc6971a4
AyaGad94 <aya.r.gad.94@gmail.com>
2026-09-14 01:25:49 +0300

    feat: configure PostgreSQL and EF Core

---
+++ backend/GuessTheNumber.Api/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using GuessTheNumber.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuessTheNumber.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(user => user.Email)
+            .IsUnique();
+    }
+}
--- backend/GuessTheNumber.Api/Program.cs
+++ backend/GuessTheNumber.Api/Program.cs
@@ -1,4 +1,9 @@
+using GuessTheNumber.Api.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
